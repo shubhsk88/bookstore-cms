b4472ebdf89f51abc7eab188f0441c7db4ce1d35
Felipe Rosa <flp.far@gmail.com>
2020-08-06 19:20:06 -0300

    Add circular percentage

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ProgressBar from 'react-customizable-progressbar';
+
 import './book.css';
+
 const Book = ({ book, handleRemoveBook }) => {
   const { title, category } = book;
 
@@ -11,7 +14,7 @@ const Book = ({ book, handleRemoveBook }) => {
         <div className="book-title">{title}</div>
         <div className="author">Suzanne Collins</div>
         <div className="book-action">
-          <button className="action">Comments</button>
+          <button type="button" className="action">Comments</button>
           <button
             type="button"
             onClick={() => handleRemoveBook(book)}
@@ -19,20 +22,17 @@ const Book = ({ book, handleRemoveBook }) => {
           >
             Remove
           </button>
-          <button className="action">Edit</button>
+          <button type="button" className="action">Edit</button>
         </div>
       </div>
       <div className="percentage-container">
-        <svg className="progress-ring" height="120" width="120">
-          <circle
-            className="progress-ring__circle"
-            stroke-width="1"
-            fill="transparent"
-            r="58"
-            cx="60"
-            cy="60"
-          />
-        </svg>
+        <ProgressBar
+          progress={64}
+          radius={34}
+          strokeWidth={6}
+          trackStrokeWidth={6}
+          strokeColor="#307bbe"
+        />
         <div className="percentage">
           <div className="number mont">64%</div>
           <div className="percentage-completed mont">Completed</div>
--- src/components/BooksList/index.js
+++ src/components/BooksList/index.js
@@ -6,17 +6,19 @@ import Book from '../Book';
 import { removeBook, changeFilter } from '../../actions';
 import CategoryFilter from '../CategoryFilter';
 
-function BooksList({ books, removeBook, changeFilter, filter }) {
-  const handleRemoveBook = (book) => {
+function BooksList({
+  books, removeBook, changeFilter, filter,
+}) {
+  const handleRemoveBook = book => {
     removeBook(book);
   };
 
-  const handleFilterChange = (filter) => {
+  const handleFilterChange = filter => {
     changeFilter(filter);
   };
   const checkfilter = (filter, books) => {
     if (filter === 'All') return books;
-    return books.filter((book) => book.category === filter);
+    return books.filter(book => book.category === filter);
   };
 
   return (
@@ -24,9 +26,9 @@ function BooksList({ books, removeBook, changeFilter, filter }) {
       <CategoryFilter handleFilterChange={handleFilterChange} />
       <table>
         <tbody>
-          {checkfilter(filter, books).map((book) => (
+          {checkfilter(filter, books).map(book => (
             <Book
-              
+
               key={book.id}
               book={book}
               handleRemoveBook={handleRemoveBook}
@@ -45,10 +47,9 @@ BooksList.propTypes = {
   filter: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   books: state.books,
   filter: state.filter,
 });
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ removeBook, changeFilter }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ removeBook, changeFilter }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
